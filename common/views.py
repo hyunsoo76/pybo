0376d1a3ab45b8b93c9a65446131b98964e641b8
정현수 <nexussu@gmail.com>
2021-08-26 13:57:17 +0900

    git test

--- common/views.py
+++ common/views.py
@@ -15,7 +15,7 @@ def signup(request):
             form.save()
             username = form.cleaned_data.get('username')
             raw_password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, password=raw_password)  # 사용자 인증
+            user = authenticate(username=username, password=raw_password)  # 사용자 인증 사용자인증
             login(request, user)  # 로그인
             return redirect('index')
     else:
