a8d334532b19f52e78777b645139058f8d5b2860
정현수 <nexussu@gmail.com>
2021-09-17 15:27:45 +0900

    base.py , 안찍혀서 추가

--- config/settings/base.py
+++ config/settings/base.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'paystub.apps.PaystubConfig'
+    'paystub.apps.PaystubConfig',
 ]
 
 MIDDLEWARE = [
