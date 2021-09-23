514d67dbfceed19ca0b0e687e1a5ab48ec192934
정현수 <nexussu@gmail.com>
2021-09-23 13:34:52 +0900

    급여명세서 페이지

--- config/settings/base.py
+++ config/settings/base.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'paystub.apps.PaystubConfig',
+    'django.contrib.humanize',
 ]
 
 MIDDLEWARE = [
--- config/urls.py
+++ config/urls.py
@@ -22,7 +22,8 @@ from pybo.views import base_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('pybo/', include('pybo.urls')),
+    path('paystub/', include('paystub.urls')),
     path('common/', include('common.urls')),
     path('', base_views.index, name='index'),  # '/' 에 해당되는 path
-    path('paystub/', include('paystub.urls')),
+    
 ]
