5258c1d2182de428621602d4f0dab417283441c9
정현수 <nexussu@gmail.com>
2022-03-25 14:12:37 +0900

    polls url update

--- polls/urls.py
+++ polls/urls.py
@@ -4,6 +4,9 @@ from . import views
 app_name = 'polls'
 
 urlpatterns = [
-    path('', views.index, name='index'),
-]
+    path('', views.index),
 
+    path('<int:pay_list_id>/',
+         views.detail, name='detail')
+
+]
