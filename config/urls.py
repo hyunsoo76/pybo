72b7a108a893a72fc845127e988c3a09ea6e4fd5
정현수 <nexussu@gmail.com>
2022-03-25 15:05:10 +0900

    polls url update

--- config/urls.py
+++ config/urls.py
@@ -17,8 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 from pybo.views import base_views
 
-
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('pybo/', include('pybo.urls')),
--- polls/urls.py
+++ polls/urls.py
@@ -1,12 +1,10 @@
 from django.urls import path
+
 from . import views
 
-app_name = 'polls'
-
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name='index'),
+]
 
-    path('<int:pay_list_id>/',
-         views.detail, name='detail')
 
 ]
