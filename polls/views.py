d8d11011e5ab64eccc0e6f607ae7ed3ae889de97
정현수 <nexussu@gmail.com>
2022-03-25 14:09:53 +0900

    polls url update

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-import django.shortcuts.render
+
 
 # Create your views here.
 def index(request):
-    return render(request, 'polls/main.html')
+
+
+    return render(request, 'polls/main.html', )
