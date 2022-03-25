caac5b109a66cf74c5daa08db0638b848c389745
정현수 <nexussu@gmail.com>
2022-03-25 17:31:33 +0900

    polls views_urls

--- polls/urls.py
+++ polls/urls.py
@@ -5,7 +5,7 @@ app_name = 'polls'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    # ex: /polls/5/
+    # ex: /polls/5
     path('<int:question_id>/', views.detail, name='detail'),
     # ex: /polls/5/results/
     path('<int:question_id>/results/', views.results, name='results'),
