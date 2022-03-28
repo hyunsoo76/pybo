ba7800aff50189a883bbfa0b1d423f9558576e2c
정현수 <nexussu@gmail.com>
2022-03-28 12:53:19 +0900

    polls index load

--- polls/views.py
+++ polls/views.py
@@ -14,10 +14,10 @@ def index(request):
 
 
 def detail(request, question_id):
-    # return HttpResponse("You're looking at question %s." % question_id)
-    def detail(request, question_id):
-        question = get_object_or_404(Question, pk=question_id)
-        return render(request, 'polls/detail.html', {'question': question})
+    return HttpResponse("You're looking at question %s." % question_id)
+    # def detail(request, question_id):
+    #     question = get_object_or_404(Question, pk=question_id)
+    #     return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
