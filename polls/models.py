d72a327f22eff861690450f301c1b54143851a27
정현수 <nexussu@gmail.com>
2022-03-28 14:48:27 +0900

    polls index load

--- polls/models.py
+++ polls/models.py
@@ -8,7 +8,7 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
 
-class Choice(models.Model):
+class Chioce(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
