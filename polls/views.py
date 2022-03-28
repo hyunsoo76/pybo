7c702da8e564934b1f7d535716a17f6acd9ec719
정현수 <nexussu@gmail.com>
2022-03-28 17:41:46 +0900

    vote page

--- polls/views.py
+++ polls/views.py
@@ -2,6 +2,8 @@ from django.template import loader
 from django.http import HttpResponse
 from .models import Question
 from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from .models import Chioce, Question
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -22,4 +24,15 @@ def results(request, question_id):
     return HttpResponse(response % question_id)
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        select_chioce = question.chioce_set.get(pk=request.POST['chioce'])
+    except(KeyError, Chioce.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        select_chioce.vote +=1
+        select_chioce.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question_id,)))
