e14f1eb353a6bc4ed6631bd842b53bfa5ea0f8e6
정현수 <nexussu@gmail.com>
2022-04-04 17:35:49 +0900

    eas 개발2

--- eas/views.py
+++ eas/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.template import loader
 
 from django.http import  HttpResponse
 
 def index(request):
+    template = loader.get_template('eas/index.html')
     return render(request, 'eas/index.html')
