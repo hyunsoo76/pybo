e1aca805ba79e905f3c71759e482b900d118e552
정현수 <nexussu@gmail.com>
2022-04-04 18:24:27 +0900

    eas 개발1qd3

--- eas/urls.py
+++ eas/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from.import views
 
 urlpatterns = [
-    path('',views.index, name='index'),
-    path('', views.detail, name='detail'),
+    path('', views.index, name='index'),
+    path('/templates/', views.detail, name='detail'),
 ]
