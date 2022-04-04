e7671be5e3dca94e7da658ad9a6bdb847146738c
정현수 <nexussu@gmail.com>
2022-04-04 18:25:53 +0900

    eas 개발4wdf5

--- eas/urls.py
+++ eas/urls.py
@@ -3,5 +3,5 @@ from.import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('/templates/', views.detail, name='detail'),
+    path('/templates/eas/', views.detail, name='detail'),
 ]
