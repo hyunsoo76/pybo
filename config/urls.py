92a6956efb681b8ea25fc3a0acb0074c238f3363
정현수 <nexussu@gmail.com>
2022-04-04 17:18:42 +0900

    eas 개발

--- config/urls.py
+++ config/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('paystub/', include('paystub.urls')),
     path('common/', include('common.urls')),
     path('polls/', include('polls.urls')),
+    path('eas/', include('eas.urls')),
     path('', base_views.index, name='index'),        # '/' 에 해당되는 path
     
 ]
