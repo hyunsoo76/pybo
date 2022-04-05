fe11962d6ef6b9571e7dc3125e6f389747687a26
정현수 <nexussu@gmail.com>
2022-04-05 20:29:49 +0900

    detail page3

--- eas/urls.py
+++ eas/urls.py
@@ -3,5 +3,5 @@ from.import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('/templates/eas/', views.detail, name='detail'),
+    path('/eas/', views.detail, name='detail'),
 ]
