37aa3823a78f125f878178964974a0008f2aebac
정현수 <nexussu@gmail.com>
2022-04-10 15:28:20 +0900

    detal

--- eas/urls.py
+++ eas/urls.py
@@ -3,6 +3,8 @@ from.import views
 from django.conf.urls.static import static
 from django.conf import settings
 
+app_name = 'eas'
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('detail', views.detail, name='detail'),
