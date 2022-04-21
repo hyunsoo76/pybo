76b2475fdeef519e7b8f9bc4f53ff23a4d730085
정현수 <nexussu@gmail.com>
2022-04-21 13:20:00 +0900

    list_page

--- eas/views.py
+++ eas/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from django.template import loader
 from django.shortcuts import get_object_or_404, render
+from .models import Request
 
 from django.http import  HttpResponse
 
 def index(request):
-    template = loader.get_template('eas/index.html')
-    return render(request, 'eas/index.html')
+    Request_list = Request.objects.order_by('-create_date')
+    context = {'Request_list': Request_list}
+    return render(request, 'eas/index.html', context)
 
 def detail(request):
     question = get_object_or_404
