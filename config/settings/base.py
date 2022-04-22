641591421de6b8a3d442b7320397318d75bde8b2
정현수 <nexussu@gmail.com>
2022-04-22 14:39:30 +0900

    detail

--- config/settings/base.py
+++ config/settings/base.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'paystub.apps.PaystubConfig',
-    'django.contrib.humanize',
+    # 'django.contrib.humanize',
     'polls.apps.PollsConfig',
     'django.contrib.humaniz',
     'eas.apps.EasConfig',
