0a05dde7e2b335098802d47e6f8e10c77763960c
정현수 <nexussu@gmail.com>
2022-04-25 10:21:23 +0900

    aaa insert

--- eas/views.py
+++ eas/views.py
@@ -33,8 +33,8 @@ def Request_create(request):
     return render(request, 'eas/detail.html', context)
 
 def detail_r_dojang(request):
-
-    return HttpResponse("안녕하세요 pybo에 오신것을 환영합니다.")
+    context = "안년하세요"
+    return render(request, 'eas/index.html', context)
 
 
 
