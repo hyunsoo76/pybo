e994188e1ad53cbe70207f449c60ffe4d3af4b14
정현수 <nexussu@gmail.com>
2022-04-25 11:28:06 +0900

    aaa insert

--- eas/urls.py
+++ eas/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path('<int:Request_id>/', views.detail),
     path('detail', views.detail, name='detail'),
     path('Request/create/', views.Request_create, name='Request_create'),
-    path('<int:new_Request_id>/', views.detail_r_dojang, name='detail_r_dojang'),
+    path('<int:Request_id>/', views.detail_r_dojang, name='detail_r_dojang'),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
