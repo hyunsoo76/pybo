56512e3b8b3acc6ac572c71414d8cbea52bc7331
정현수 <nexussu@gmail.com>
2022-04-26 10:31:03 +0900

    dojang

--- eas/views.py
+++ eas/views.py
@@ -42,7 +42,7 @@ def detail_update(request, new_Request_id):
             new_Request.aaa = request.POST.get('input_reject')
             new_Request.save()
             context = {'new_Request': new_Request}
-            return render(request, 'eas/detail.html', context)
+            return render(request, 'eas/detail_r.html', context)
             # return redirect('eas:detail.html', new_Request_id=new_Request.id)
     else:
         form = RequestForm(request.POST, instance=new_Request)
