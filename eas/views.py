be7ad78cf5d1b090aac6bf0d93af5db47afb62d8
정현수 <nexussu@gmail.com>
2022-04-29 16:21:00 +0900

    list

--- eas/views.py
+++ eas/views.py
@@ -7,15 +7,60 @@ from .models import Request
 from .forms import RequestForm
 from django.http import  HttpResponse
 from django.core.paginator import Paginator
+from django.db.models import Q
 
 
 def index(request):
     page = request.GET.get('page', '1')  # 페이지
+    kw = request.GET.get('kw', '')  # 검색어
     Request_list = Request.objects.order_by('-create_date')
+    if kw:
+        Request_list = Request_list.filter(
+            Q(subject__icontains=kw) |  # 제목 검색
+            Q(a_1__icontains=kw) |  # 매입처명
+            Q(b_1__icontains=kw) |
+            Q(c_1__icontains=kw) |
+            Q(d_1__icontains=kw) |
+            Q(e_1__icontains=kw) |
+            Q(f_1__icontains=kw) |
+            Q(g_1__icontains=kw) |
+            Q(h_1__icontains=kw) |
+            Q(i_1__icontains=kw) |
+            Q(j_1__icontains=kw) |
+            Q(a_4__icontains=kw) |  # 계좌명
+            Q(b_4__icontains=kw) |
+            Q(d_4__icontains=kw) |
+            Q(e_4__icontains=kw) |
+            Q(f_4__icontains=kw) |
+            Q(g_4__icontains=kw) |
+            Q(h_4__icontains=kw) |
+            Q(i_4__icontains=kw) |
+            Q(j_4__icontains=kw) |
+            Q(a_5__icontains=kw) |  # 금액
+            Q(b_5__icontains=kw) |
+            Q(c_5__icontains=kw) |
+            Q(d_5__icontains=kw) |
+            Q(e_5__icontains=kw) |
+            Q(f_5__icontains=kw) |
+            Q(g_5__icontains=kw) |
+            Q(h_5__icontains=kw) |
+            Q(i_5__icontains=kw) |
+            Q(j_5__icontains=kw) |
+            Q(a_7__icontains=kw) |  # 비고
+            Q(b_7__icontains=kw) |
+            Q(c_7__icontains=kw) |
+            Q(d_7__icontains=kw) |
+            Q(e_7__icontains=kw) |
+            Q(f_7__icontains=kw) |
+            Q(g_7__icontains=kw) |
+            Q(h_7__icontains=kw) |
+            Q(i_7__icontains=kw) |
+            Q(j_7__icontains=kw)
+        ).distinct()
     # context = {'Request_list': Request_list}
     paginator = Paginator(Request_list, 10)  # 페이지당 10개씩 보여주기
     page_obj = paginator.get_page(page)
-    context = {'Request_list': page_obj}
+    context = {'Request_list': page_obj, 'page': page, 'kw': kw}
     return render(request, 'eas/index.html', context)
 
 def detail(request, Request_id):
