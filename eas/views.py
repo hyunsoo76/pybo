d1b2d556493dc30dc192d87242316348c2ffbb41
정현수 <nexussu@gmail.com>
2022-05-06 09:26:39 +0900

    modify

--- eas/views.py
+++ eas/views.py
@@ -158,8 +158,7 @@ def Request_modify(request, new_Request_id):
             new_Request.create_date = timezone.now()  # 수정일시 저장
             new_Request.save()
             # return redirect(request.META.get('HTTP_REFERER', 'redirect_if_referer_not_found'))
-            return redirect('views.detail')
-
+            return redirect('eas:detail_r')
     else:
         form = RequestForm(instance=new_Request)
         context = {'form': form}
