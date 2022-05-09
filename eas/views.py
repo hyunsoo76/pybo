9bf3fe318ea4af0a8a69cdf1959d4cc9461b5000
정현수 <nexussu@gmail.com>
2022-05-09 12:24:18 +0900

    test

--- eas/views.py
+++ eas/views.py
@@ -133,9 +133,9 @@ def detail_update2(request, new_Request_id):
             new_Request.bbb = temp
             new_Request.date2 = timezone.now()
             new_Request.save()
-            if new_Request.bbb == "승인":
-                from eas import pushmsg
-                subprocess.call(pushmsg)
+            # if new_Request.bbb == "승인":
+            from eas import pushmsg
+            subprocess.call(pushmsg)
             messages.warning(request, "결재완료")
             return redirect(request.META.get('HTTP_REFERER', 'redirect_if_referer_not_found'))
         else:
