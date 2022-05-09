018f88fcb9dfb881ccf6e24c1331c067da83d24d
정현수 <nexussu@gmail.com>
2022-05-09 18:00:54 +0900

    test

--- eas/views.py
+++ eas/views.py
@@ -4,12 +4,14 @@ from django.contrib import messages
 from django.shortcuts import render, redirect
 from django.template import loader
 from django.shortcuts import get_object_or_404, render
+
+from . import pushmsg
 from .models import Request
 from .forms import RequestForm
 from django.http import  HttpResponse
 from django.core.paginator import Paginator
 from django.db.models import Q
-import eas
+
 
 
 
@@ -132,12 +134,12 @@ def detail_update2(request, new_Request_id):
             new_Request.bbb = temp
             new_Request.date2 = timezone.now()
             new_Request.save()
-            eas.pushmsg.main()
+            pushmsg.main()
             messages.warning(request, "결재완료")
             return redirect(request.META.get('HTTP_REFERER', 'redirect_if_referer_not_found'))
         else:
             return redirect(request.META.get('HTTP_REFERER', 'redirect_if_referer_not_found'))
-    eas.pushmsg.main()
+    pushmsg.main()
     # subprocess.run(['eas/pushmsg.py'])
 
 def detail_okupdate2(request, new_Request_id):
