7c1dae13d2c63b05748e4d44215f1d622e51df5b
정현수 <nexussu@gmail.com>
2022-05-09 12:29:24 +0900

    test

--- eas/views.py
+++ eas/views.py
@@ -10,7 +10,7 @@ from .forms import RequestForm
 from django.http import  HttpResponse
 from django.core.paginator import Paginator
 from django.db.models import Q
-
+from eas import pushmsg
 
 
 
@@ -133,9 +133,8 @@ def detail_update2(request, new_Request_id):
             new_Request.bbb = temp
             new_Request.date2 = timezone.now()
             new_Request.save()
-            # if new_Request.bbb == "승인":
-            from eas import pushmsg
-            subprocess.run(pushmsg)
+            if new_Request.bbb == "승인":
+               subprocess.run(pushmsg)
             messages.warning(request, "결재완료")
             return redirect(request.META.get('HTTP_REFERER', 'redirect_if_referer_not_found'))
         else:
