dbd4a38f694588c1b721cf035258ad8d371b8061
정현수 <nexussu@gmail.com>
2022-05-09 12:59:17 +0900

    test

--- eas/views.py
+++ eas/views.py
@@ -134,8 +134,8 @@ def detail_update2(request, new_Request_id):
             new_Request.bbb = temp
             new_Request.date2 = timezone.now()
             new_Request.save()
-            if new_Request.bbb == "승인":
-               subprocess.run('pushmsg.py')
+            # if new_Request.bbb == "승인":
+            subprocess.run('pushmsg.py')
             messages.warning(request, "결재완료")
             return redirect(request.META.get('HTTP_REFERER', 'redirect_if_referer_not_found'))
         else:
