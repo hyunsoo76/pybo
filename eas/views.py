4a428101537fdceaf99b0062b875a2afc4ac3448
정현수 <nexussu@gmail.com>
2022-05-09 15:33:20 +0900

    test

--- eas/views.py
+++ eas/views.py
@@ -136,7 +136,7 @@ def detail_update2(request, new_Request_id):
             return redirect(request.META.get('HTTP_REFERER', 'redirect_if_referer_not_found'))
         else:
             return redirect(request.META.get('HTTP_REFERER', 'redirect_if_referer_not_found'))
-    subprocess.call(['/home/ubuntu/projects/mysite/eas/pushmsg.py'])
+    subprocess.call(['eas/pushmsg.py'])
 
 def detail_okupdate2(request, new_Request_id):
     new_Request = get_object_or_404(Request, pk=new_Request_id)
