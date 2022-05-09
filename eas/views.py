da6ffcf3f79f4d867ea8699151293cec994ff927
정현수 <nexussu@gmail.com>
2022-05-09 18:03:21 +0900

    test

--- eas/views.py
+++ eas/views.py
@@ -135,6 +135,8 @@ def detail_update2(request, new_Request_id):
             new_Request.date2 = timezone.now()
             new_Request.save()
             pushmsg.main()
+            subprocess.run(pushmsg.main())
+
             messages.warning(request, "결재완료")
             return redirect(request.META.get('HTTP_REFERER', 'redirect_if_referer_not_found'))
         else:
