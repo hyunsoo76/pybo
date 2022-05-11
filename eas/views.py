d9738da0cde4076ac9d3fbb35ddb2ff8e61667b4
정현수 <nexussu@gmail.com>
2022-05-11 18:34:34 +0900

    push

--- eas/views.py
+++ eas/views.py
@@ -13,6 +13,8 @@ from django.core.paginator import Paginator
 from django.db.models import Q
 
 from eas import pushmsg
+from .pushmsg import main
+
 
 def index(request):
     page = request.GET.get('page', '1')  # 페이지
@@ -146,8 +148,8 @@ def detail_update2(request, new_Request_id):
             new_Request.save()
             # if new_Request.bbb == "승인":
             #     pushmsg.push()
-            # pushmsg.main()
-            subprocess.run(pushmsg.main())
+            main();
+            # subprocess.run(pushmsg.main())
             return redirect(request.META.get('HTTP_REFERER', 'redirect_if_referer_not_found'))
         else:
             return redirect(request.META.get('HTTP_REFERER', 'redirect_if_referer_not_found'))
