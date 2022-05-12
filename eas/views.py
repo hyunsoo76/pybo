22ce563886055839ee6cde0e9a3b2993126566fa
정현수 <nexussu@gmail.com>
2022-05-12 13:27:29 +0900

    push

--- eas/views.py
+++ eas/views.py
@@ -164,8 +164,9 @@ def detail_okupdate2(request, new_Request_id):
             new_Request.bbb = temp
             new_Request.date2 = timezone.now()
             new_Request.save()
-            if __name__ == '__main__':
-                main()
+            from.pushmsg import reload
+            # if __name__ == '__main__':
+            reload(pushmsg)
             return redirect(request.META.get('HTTP_REFERER', 'redirect_if_referer_not_found'))
 
         else:
