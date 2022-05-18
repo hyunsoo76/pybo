3352bddf30d50f142c09dda5374ee9804980d5eb
정현수 <nexussu@gmail.com>
2022-05-18 17:31:06 +0900

    accesskey

--- eas/views.py
+++ eas/views.py
@@ -118,7 +118,7 @@ def Request_create(request):
 
             # return redirect('eas:index')
     else:
-        query = request.GET.get['a_1']
+        query = request.GET.get('a_1')
         if query:
             s_result = Request.object.fillter(a_1__contains=query)
         return render(request, 'detail.html', {'s_result': s_result})
