b192a4a8ee031ac5b7883fe0e86ceddfc576e745
정현수 <nexussu@gmail.com>
2022-06-06 16:18:45 +0900

    nomal_approval

--- eas/views.py
+++ eas/views.py
@@ -393,11 +393,12 @@ def account(request):
 # 일반품의 insert
 def nomal_approval(request):
     if request.method == 'POST':
-        temp = request.POST.get('nomal')
+
         form = RequestForm(request.POST)
         if form.is_valid():
             new_Request = form.save(commit=False)
             new_Request.create_date = timezone.now()
+            temp = request.POST.get('nomal')
             new_Request.jisi1 = temp
             new_Request.save()
             # context = {'new_Request': new_Request}
