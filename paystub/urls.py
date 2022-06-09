afbdcc6df959872257ab531adae874d5a44f80a7
정현수 <nexussu@gmail.com>
2022-06-09 09:37:29 +0900

    1

--- paystub/urls.py
+++ paystub/urls.py
@@ -6,6 +6,6 @@ app_name = 'paystub'
 urlpatterns = [
     path('', views.index),
 
-    path('<int:pay_list_id>/',
-         views.detail, name='detail')
+    # path('<int:pay_list_id>/',
+    #      views.detail, name='detail')
 ]
