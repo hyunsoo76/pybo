2a65f4b2b5678554ab93eb1ec2c26a623970f98a
정현수 <nexussu@gmail.com>
2022-06-23 18:42:23 +0900

    eos app start

--- eos/views.py
+++ eos/views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse
-
+from django.shortcuts import redirect
 def index(request):
-    return HttpResponse("https://picsum.photos/1080/620/?random")
+    return redirect('https://picsum.photos/1080/620/?random')
