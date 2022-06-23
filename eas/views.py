064e13be040f619b4977b95169cf44944c70583a
정현수 <nexussu@gmail.com>
2022-06-23 14:43:58 +0900

    index counte

--- eas/views.py
+++ eas/views.py
@@ -100,26 +100,26 @@ def detail(request, Request_id):
                             totalsum = totalsum + total
 
                     new_Request.total = totalsum
-                    # 외 몇개의 매입처인지 표기하기기 위해
-                    if new_Request.b_1:
-                        eee = "2"
-                    elif new_Request.c_1:
-                        eee = "3"
-                    elif new_Request.d_1:
-                        eee = "4"
-                    elif new_Request.e_1:
-                        eee = "5"
-                    elif new_Request.f_1:
-                        eee = "6"
-                    elif new_Request.g_1:
-                        eee = "7"
-                    elif new_Request.h_1:
-                        eee = "8"
-                    elif new_Request.i_1:
-                        eee = "9"
-                    elif new_Request.j_1:
-                        eee = "10"
-                    new_Request.eee = eee
+                    # # 외 몇개의 매입처인지 표기하기기 위해
+                    # if new_Request.b_1:
+                    #     eee = "2"
+                    # elif new_Request.c_1:
+                    #     eee = "3"
+                    # elif new_Request.d_1:
+                    #     eee = "4"
+                    # elif new_Request.e_1:
+                    #     eee = "5"
+                    # elif new_Request.f_1:
+                    #     eee = "6"
+                    # elif new_Request.g_1:
+                    #     eee = "7"
+                    # elif new_Request.h_1:
+                    #     eee = "8"
+                    # elif new_Request.i_1:
+                    #     eee = "9"
+                    # elif new_Request.j_1:
+                    #     eee = "10"
+                    # new_Request.eee = eee
 
                     context = {'new_Request': new_Request}
                     return render(request, 'eas/detail_r.html', context)
