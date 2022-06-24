e36869310a9b13213de8dbce036bd26ac485db56
정현수 <nexussu@gmail.com>
2022-06-24 14:24:41 +0900

    eosindex

--- eos/views.py
+++ eos/views.py
@@ -1,6 +1,5 @@
-from django.http import HttpResponse
-from django.shortcuts import redirect, render
+from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'eos/index.html')
