ee0e29d211eb4fe79fad0d4174cba41464477005
정현수 <nexussu@gmail.com>
2022-06-24 14:00:13 +0900

    eosindex

--- eos/urls.py
+++ eos/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-
 from . import views
+
+app_name = 'eos'
 
 urlpatterns = [
     path('', views.index, name='index'),
--- eos/views.py
+++ eos/views.py
@@ -1,4 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import redirect
+
+
 def index(request):
     HttpResponse('eos/index.html')
