0202e5c9bf99f53829520a525985db7d7357ea93
정현수 <nexussu@gmail.com>
2022-06-24 14:26:30 +0900

    eosindex

--- eos/views.py
+++ eos/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import redirect, render
 
 
 def index(request):
-    return render(request, 'eos/index.html')
+    # return render(request, 'eos/index.html')
+    return HttpResponse("hellow world")
