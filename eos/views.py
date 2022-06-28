07a36b56475f96d6647b5aa913c7277b9e60d5dd
정현수 <nexussu@gmail.com>
2022-06-28 15:06:37 +0900

    eos index

--- eos/views.py
+++ eos/views.py
@@ -3,5 +3,4 @@ from django.shortcuts import redirect, render
 
 
 def index(request):
-    # return redirect('https://picsum.photos/1080/620/?random')
-    return render(request,'http://3.37.211.248/eos')
+    return redirect('https://picsum.photos/1080/620/?random')
