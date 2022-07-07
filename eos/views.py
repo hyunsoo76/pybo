94b01885303024cc74ec32dd9447a362d3c8a7b2
정현수 <nexussu@gmail.com>
2022-07-07 16:26:24 +0900

    dd

--- eos/views.py
+++ eos/views.py
@@ -19,12 +19,12 @@ class ProductView(ListView):
 
 # 상품db 업데이트
 
-# with open(‘d:/p_list.csv’,‘r’) as f:
-#     dr = csv.DictReader(f)
-#     s = pd.DataFrame(dr)
-# ss = []
-# for i in range(len(s)):
-#     st = (s[‘회사명’][i], s[‘종목코드’][i], s[‘상장일’][i])
-#     ss.append(st)
-# for i in range(len(s)):
-#     Stock.objects.create(name=ss[i][0], code=ss[i][1], ipo_date=ss[i][2])
+with open(‘/home/ubuntu/projects/mysite/static/p_list.csv’,‘r’) as f:
+    dr = csv.DictReader(f)
+    s = pd.DataFrame(dr)
+ss = []
+for i in range(len(s)):
+    st = (s[‘회사명’][i], s[‘종목코드’][i], s[‘상장일’][i])
+    ss.append(st)
+for i in range(len(s)):
+    Stock.objects.create(name=ss[i][0], code=ss[i][1], ipo_date=ss[i][2])
