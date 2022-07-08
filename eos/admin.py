d06581db6bc55f863aefeef514fa5d542ddc6631
정현수 <nexussu@gmail.com>
2022-07-08 15:00:09 +0900

    p_list csv upload_view

--- eos/admin.py
+++ eos/admin.py
@@ -8,14 +8,14 @@ from .models import Products
 class Order_listAdmin(admin.ModelAdmin):
     search_fields = ['d_day']
 
-# class UserAdmin(admin.ModelAdmin):
-#     search_fields = ['buyer_name']
+class UserAdmin(admin.ModelAdmin):
+    search_fields = ['buyer_name']
 
 class ProductsAdmin(admin.ModelAdmin):
     search_fields = ['p_name']
     list_display = ('p_name', 'org_bar', 'sale_bar', 'p_price')
 
 admin.site.register(Order_list, Order_listAdmin)
-# admin.site.register(User, UserAdmin)
+admin.site.register(User, UserAdmin)
 admin.site.register(Products, ProductsAdmin)
 
