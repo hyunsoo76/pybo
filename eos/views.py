98645f2c79dc3167545cb78563b7c6123a8650d2
정현수 <nexussu@gmail.com>
2022-07-13 17:42:53 +0900

    20220713

--- eos/views.py
+++ eos/views.py
@@ -73,6 +73,7 @@ def order_create(request):
             odbox = request.POST.get('od_box_count_input')
             if odbox != '' :
                 new_order_list.fff = barcode
+                new_order_list.od_count = 0
                 new_order_list.od_box_count = odbox
                 new_order_list.save()
                 new_user.save()
