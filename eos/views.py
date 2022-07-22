4ffa6f23a7df68a4c153eb0d9e96f4a2fbe06adf
정현수 <nexussu@gmail.com>
2022-07-22 17:40:46 +0900

    eos create view

--- eos/views.py
+++ eos/views.py
@@ -123,7 +123,8 @@ def order_create(request):
             new_order_list.save()
 
             if new_order_list.buyer_name == '':
-                some_function(request)
+                # some_function(request)
+                messages.error(request, "발주매장 선택하세요")
                 form = form
                 context = {'form': form}
                 return render(request, 'eos/order_page.html', context)
@@ -162,7 +163,7 @@ def order_create(request):
 
 def some_function(request):
     messages.error(request, "발주매장 선택하세요")
-    order_create(request)
+    # order_create(request)
 # 메시지 팝업
 # def some_function(request):
 #     messages.warning(request, "낱개발주와 박스발주 같이 입력하면 낱개발주 0 으로 됨.")
