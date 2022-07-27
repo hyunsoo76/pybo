5df8b224d2d7a3553204487b188f60ab4a9f4189
정현수 <nexussu@gmail.com>
2022-07-27 14:14:27 +0900

    order_list_r

--- eos/views.py
+++ eos/views.py
@@ -139,13 +139,14 @@ def order_create(request):
             new_order_list.od_list = data
 
             # Product Class 조회 변수 저장
-            psb = Products.objects.get(sale_bar=(new_order_list.od_list[0]))
-            new_order_list.s_product = psb.p_name
-            new_order_list.s_iq = psb.iq
-            new_order_list.s_price = psb.p_price
-            new_order_list.s_location = psb.location
-            new_order_list.s_org_bar = psb.org_bar
-            new_order_list.save()
+            for i in list :
+                psb = Products.objects.get(sale_bar=(new_order_list.od_list[i]))
+                new_order_list.s_product = psb.p_name[i]
+                new_order_list.s_iq = psb.iq[i]
+                new_order_list.s_price = psb.p_price[i]
+                new_order_list.s_location = psb.location[i]
+                new_order_list.s_org_bar = psb.org_bar[i]
+                new_order_list.save()
             context = {'new_order_list': new_order_list}
             return render(request, 'eos/order_page_r.html', context)
         else:
