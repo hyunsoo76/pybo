9dee525a066eac66472d33c89559dc44d0d61e84
정현수 <nexussu@gmail.com>
2022-07-27 11:19:32 +0900

    order_list_r

--- eos/views.py
+++ eos/views.py
@@ -140,7 +140,7 @@ def order_create(request):
 
             # Product Class 조회 변수 저장
             psb = Products.objects.filter(sale_bar = (new_order_list.od_list[0]))
-            new_order_list.fff = psb
+            new_order_list.fff = psb.sale_bar
             new_order_list.save()
             context = {'new_order_list': new_order_list}
             return render(request, 'eos/order_page_r.html', context)
