58f18c9309e33af3f12bb8dfe6819b64bc155439
정현수 <nexussu@gmail.com>
2022-07-27 09:34:39 +0900

    order_list_r

--- eos/admin.py
+++ eos/admin.py
@@ -10,7 +10,7 @@ class Order_listAdmin(admin.ModelAdmin):
 
 class ProductsAdmin(admin.ModelAdmin):
     search_fields = ['p_name']
-    list_display = ('p_name', 'org_bar', 'sale_bar', 'p_price')
+    list_display = ('p_name', 'org_bar', 'sale_bar', 'p_price', 'iq', 'location')
 
 admin.site.register(Order_list, Order_listAdmin)
 admin.site.register(Products, ProductsAdmin)
