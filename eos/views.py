cfe9f410f08145d028139636497a167614419e19
정현수 <nexussu@gmail.com>
2022-07-29 11:36:05 +0900

    ajax

--- eos/views.py
+++ eos/views.py
@@ -173,7 +173,7 @@ def some_function(request):
 def searchData(request):
     # POST 요청일 때
     if request.method == 'POST':
-        data = request.POST['searchwords']
+        data = request.POST.get('searchwords', False)
         # sdata = Products.objects.get(sale_bar=(data))
         context = {
             'results': data,
