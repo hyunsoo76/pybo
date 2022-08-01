b35bf5459886640c0f1568c51283609fe444369d
정현수 <nexussu@gmail.com>
2022-08-01 16:58:33 +0900

    ajax

--- eos/views.py
+++ eos/views.py
@@ -185,6 +185,8 @@ def searchData(request):
         }
         # context = "리스테린"
         # context = get_Search_Results(inputdata)
-        # return HttpResponse(context)
-        return JsonResponse(context)
+        json = dumps(context, cls=DjangoJSONEncoder)
+        return HttpResponse(json)
+        
+        # return JsonResponse(context)
         # return HttpResponse(json.dumps(context), content_type="application/json")
