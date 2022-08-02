8257aee5ef7728d18185f2a700e518d690d089a8
정현수 <nexussu@gmail.com>
2022-08-02 09:27:25 +0900

    ajax

--- eos/views.py
+++ eos/views.py
@@ -174,7 +174,7 @@ def searchData(request):
     # POST 요청일 때
     if 'searchwords' in request.POST:
         data = request.POST['searchwords']
-        s_data = Products.objects.filter(sale_bar=(data))
+        s_data = Products.objects.get(sale_bar=(data))
         inputdata = s_data.p_name
 
         context = {
