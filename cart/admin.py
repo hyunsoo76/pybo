bb0765e4305c5981b87a54d8ec242ff3dab23654
정현수 <nexussu@gmail.com>
2022-08-17 17:34:22 +0900

    cart caeat

--- cart/admin.py
+++ cart/admin.py
@@ -6,11 +6,10 @@ from .models import CartItem
 
 class CartAdmin(admin.ModelAdmin):
     search_fields = ['cart_id', 'date_added']
-    list_display = ('date_added')
 
 class CartItemAdmin(admin.ModelAdmin):
     search_fields = ['product', 'cart']
-    list_display = ('product', 'cart', 'quantity', 'active')
+    list_display = ('product', 'product_p_name', 'cart', 'quantity', 'active')
 
 admin.site.register(Cart, CartAdmin)
 admin.site.register(CartItem, CartItemAdmin)
