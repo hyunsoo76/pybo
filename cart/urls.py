08c1defb378d205a77ef968c558507c310197c83
정현수 <nexussu@gmail.com>
2022-08-17 16:34:28 +0900

    cart caeat

--- cart/urls.py
+++ cart/urls.py
@@ -4,6 +4,6 @@ from .import views
 app_name = 'cart'
 
 urlpatterns = [
-    # path('add/<int:product_id>/', views.add_cart, name='add_cart'),
-    # path('', views.cart_detail, name='cart_detail'),
+    path('add/<int:product_id>/', views.add_cart, name='add_cart'),
+    path('', views.cart_detail, name='cart_detail'),
 ]
