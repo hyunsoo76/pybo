d081035fbcdb9b7e9d7a41b2a59de3d0da6e7001
정현수 <nexussu@gmail.com>
2022-08-31 18:52:07 +0900

    cart-test

--- cart/views.py
+++ cart/views.py
@@ -48,8 +48,8 @@ def cart_detail(request, total=0,counter=0, cart_items = None):
     return render(request, 'cart/cart.html', dict(cart_items=cart_items, total=total, counter=counter))
 
 
-def cart_test(request, Cart_id, cart_item=None):
+def cart_test(request, Cart_id):
     cart = get_object_or_404(Cart, pk=Cart_id)
     cart_items = CartItem.objects.filter(cart=cart, active=True)
-    esum = cart_items.quantity
+
     return render(request, 'eos/order_list.html', dict(cart_items=cart_items, esum=esum))
