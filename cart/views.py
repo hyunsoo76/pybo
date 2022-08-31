44bc755f5be3123fc3cf7592cae37c6cff8e1598
정현수 <nexussu@gmail.com>
2022-08-31 15:03:04 +0900

    cart-test

--- cart/views.py
+++ cart/views.py
@@ -49,12 +49,6 @@ def cart_detail(request, total=0,counter=0, cart_items = None):
 
 
 def cart_test(request, Cart_id):
-    try:
-        cart = get_object_or_404(Cart, pk=Cart_id)
-        cart_items = CartItem.objects.filter(cart=cart, active=True)
-        # for cart_item in cart_items:
-        #     total += (cart_item.product.p_price * cart_item.quantity)
-        #     counter += cart_item.quantity
-    except ObjectDoesNotExist:
-        pass
+    cart = get_object_or_404(Cart, pk=Cart_id)
+    cart_items = CartItem.objects.filter(cart=cart, active=True)
     return render(request, 'cart/cart_test.html', dict(cart_items=cart_items, total=total, counter=counter))
