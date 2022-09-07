8c85d10cae351177bfa1439b83b58b474647a88a
정현수 <nexussu@gmail.com>
2022-09-07 11:27:29 +0900

    eosviewcart

--- eos/views.py
+++ eos/views.py
@@ -170,6 +170,29 @@ def _cart_id(request):
         cart = request.session.create()
     return cart
 
+def add_cart(request,product_id):
+    product = Products.objects.get(id=product_id)
+    try:
+        cart = Cart.objects.get(_cart_id=_cart_id(request))
+    except Cart.DoesNotExist:
+        cart = Cart.objects.create(
+            cart_id=_cart_id(request)
+        )
+        cart.save()
+
+    try:
+        cart_item = CartItem.objects.get(product=product, cart=cart)
+        cart_item.quantity += 1
+        cart_item.save()
+    except CartItem.DoesNotExist:
+        cart_item = CartItem.objects.create(
+            product=product,
+            quantity=1,
+            cart=cart
+        )
+        cart_item.save()
+    return redirect('order_create')
+
 def order_create(request, Cart_id):
     if request.method == 'POST':
         form = Order_listForm(request.POST)
