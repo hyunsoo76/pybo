7aa4f5259804917235ac37d9db10d4e609d3e0e8
정현수 <nexussu@gmail.com>
2023-10-20 17:47:32 +0900

    view

--- eas/views.py
+++ eas/views.py
@@ -89,8 +89,8 @@ def detail(request, Request_id):
                 if total != None:
                     totalsum = totalsum + total
 
-            new_Request.total = totalsum            
-            detail_modify()
+            new_Request.total = totalsum
+
             context = {'new_Request': new_Request}
             return render(request, 'eas/detail_r_24.html', context)
         else:
@@ -153,7 +153,6 @@ def Request_create(request):
             # new_Request.save()
             context = {'new_Request': new_Request}
             return render(request, 'eas/detail_r.html', context)
-            
         else:
             form = RequestForm(request.POST)
             context = {'form': form}
