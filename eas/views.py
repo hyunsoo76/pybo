870e4bee68de5034f8df8d9e299d1addd4fe4c49
정현수 <nexussu@gmail.com>
2023-10-20 11:19:04 +0900

    142line new_Request.save()를 152line로 이동

--- eas/views.py
+++ eas/views.py
@@ -139,7 +139,7 @@ def Request_create(request):
             elif new_Request.b_1:
                 new_Request.fff = 1
 
-            new_Request.save()
+            
             totals = [new_Request.a_5, new_Request.b_5, new_Request.c_5,
                       new_Request.d_5, new_Request.e_5, new_Request.f_5,
                       new_Request.g_5, new_Request.h_5, new_Request.i_5,
@@ -149,7 +149,7 @@ def Request_create(request):
                 if total != None:
                     totalsum = totalsum + total
             new_Request.total = totalsum
-
+            new_Request.save()
             context = {'new_Request': new_Request}
             return render(request, 'eas/detail_r.html', context)
         else:
