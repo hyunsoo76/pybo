9525d840db528892e41afcaa5576f301645e34a7
정현수 <nexussu@gmail.com>
2023-10-20 15:58:56 +0900

    view 108line insert

--- eas/views.py
+++ eas/views.py
@@ -105,6 +105,7 @@ def detail(request, Request_id):
                         totalsum = totalsum + total
 
                 new_Request.total = totalsum
+                new_Request.total = new_Request.a_5 + new_Request.b_5 + new_Request.c_5 + new_Request.d_5 + new_Request.e_5 + new_Request.f_5 + new_Request.g_5 + new_Request.h_5 + new_Request.i_5 + new_Request.j_5
                 new_Request.save()
                 context = {'new_Request': new_Request}
                 return render(request, 'eas/detail_r.html', context)
