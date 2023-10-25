aebfced162da90d7947d2abaf408cad9efe7a165
정현수 <nexussu@gmail.com>
2023-10-25 16:15:48 +0900

    뷰 라인 89 int로 수정

--- eas/views.py
+++ eas/views.py
@@ -150,7 +150,6 @@ def Request_create(request):
                 if total == None:
                     totalsum = totalsum + total
             new_Request.total = totalsum
-            new_Request.total = new_Request.a_5 + new_Request.b_5 + new_Request.c_5 + new_Request.d_5+ new_Request.e_5+ new_Request.f_5+new_Request.g_5+ new_Request.h_5+ new_Request.i_5+new_Request.j_5
             new_Request.save()
             context = {'new_Request': new_Request}
             return render(request, 'eas/detail_r.html', context)
