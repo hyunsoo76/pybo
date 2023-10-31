c401fa1d27f39a44070eb4f49372d00016a642f6
정현수 <nexussu@gmail.com>
2023-10-31 16:21:51 +0900

    view 153

--- eas/views.py
+++ eas/views.py
@@ -150,7 +150,7 @@ def Request_create(request):
             #     if total != None:
             #         totalsum = totalsum + total
             
-            new_Request.total = new_Request.a_5 + new_Request.b_5
+            new_Request.total = 1111
             new_Request.save()
             context = {'new_Request': new_Request}
             return render(request, 'eas/detail_r.html', context)
