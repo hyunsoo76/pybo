d26a0e2da5835c130c6dbff0d4c4418dd9d2f812
Jeong hyun soo <nexussu@gmail.com>
2026-04-08 09:21:12 +0900

    Sync settings host config with server

--- config/settings/local.py
+++ config/settings/local.py
@@ -4,3 +4,10 @@ import os
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 ALLOWED_HOSTS = ['*']
+
+# --- vendor autocomplete API allow hosts ---
+ALLOWED_HOSTS = list(set((globals().get("ALLOWED_HOSTS") or []) + [
+    "3.37.211.248",
+    "127.0.0.1",
+    "localhost",
+]))
--- config/settings/prod.py
+++ config/settings/prod.py
@@ -4,3 +4,10 @@ ALLOWED_HOSTS = ['3.37.211.248']
 STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
 DEBUG = True
+
+# --- Allow hosts for vendor autocomplete API / health checks ---
+ALLOWED_HOSTS = list(set((globals().get("ALLOWED_HOSTS") or []) + [
+    "3.37.211.248",
+    "127.0.0.1",
+    "localhost",
+]))
