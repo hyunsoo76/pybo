dd1bd7987044d9c6e47d851676f3633dbf1e0c5c
Jeong hyun soo <nexussu@gmail.com>
2026-04-09 16:17:43 +0900

    leave DB 연결 추가

--- config/settings/base.py
+++ config/settings/base.py
@@ -91,9 +91,12 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    },
+    'leave': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': '/Users/jeong/project/dev/leave/db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
