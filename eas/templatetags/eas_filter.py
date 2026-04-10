607b0633b772d9c45a652fdc7399fcf6220596a6
Jeong hyun soo <nexussu@gmail.com>
2026-04-10 09:27:25 +0900

    EAS index 잘못된 font-color 속성 정리

--- eas/templatetags/eas_filter.py
+++ eas/templatetags/eas_filter.py
@@ -1,8 +1,28 @@
 from django import template
 
+
 register = template.Library()
 
 
 @register.filter
 def sub(value, arg):
     return value - arg
+
+from django.utils.safestring import mark_safe
+import re
+
+
+@register.filter
+def highlight(text, keyword):
+    if text is None:
+        return ""
+
+    text = str(text)
+    keyword = (keyword or "").strip()
+
+    if not keyword:
+        return text
+
+    pattern = re.compile(re.escape(keyword), re.IGNORECASE)
+    highlighted = pattern.sub(lambda m: f"<mark>{m.group(0)}</mark>", text)
+    return mark_safe(highlighted)
